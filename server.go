ca8612953a1a4767188597fe5db6b0a00702c43d
Travis Cline <travis.cline@gmail.com>
2021-08-23 19:52:45 -0700

    moderncrud: Add basic server

---
+++ server.go
@@ -0,0 +1,22 @@
+package moderncrud
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "hello, world")
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Handling HTTP requests on %s.", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
