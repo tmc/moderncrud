6438027b6df9f3adb29e71c8feb17955866beb18
Travis Cline <travis.cline@gmail.com>
2021-08-23 19:30:36 -0700

    ent: Add fields to Widget

--- ent/schema/widget.go
+++ ent/schema/widget.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Widget holds the schema definition for the Widget entity.
 type Widget struct {
@@ -9,7 +14,18 @@ type Widget struct {
 
 // Fields of the Widget.
 func (Widget) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Text("note").
+			NotEmpty(),
+		field.Time("created_at").
+			Default(time.Now).
+			Immutable(),
+		field.Enum("status").
+			Values("draft", "completed").
+			Default("draft"),
+		field.Int("priority").
+			Default(0),
+	}
 }
 
 // Edges of the Widget.
