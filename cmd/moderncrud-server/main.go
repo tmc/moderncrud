ecd8b3c0a5bceee858959575073f85223bf7b6e4
Travis Cline <travis.cline@gmail.com>
2021-08-23 20:07:55 -0700

    moderncrud-server: Add postgres support

--- cmd/moderncrud-server/main.go
+++ cmd/moderncrud-server/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/tmc/moderncrud/ent"
 )
 
 func main() {
+	client := newConnection()
+	ctx := context.Background()
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello, world")
 	})
@@ -19,4 +32,41 @@ func main() {
 
 	log.Printf("Handling HTTP requests on %s.", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+// Open new connection to the database.
+func newConnection() *ent.Client {
+	var (
+		dbUser                 = mustGetenv("DB_USER")                  // e.g. 'my-db-user'
+		dbPwd                  = mustGetenv("DB_PASS")                  // e.g. 'my-db-password'
+		instanceConnectionName = mustGetenv("INSTANCE_CONNECTION_NAME") // e.g. 'project:region:instance'
+		dbName                 = mustGetenv("DB_NAME")                  // e.g. 'my-database'
+	)
+
+	socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+	if !isSet {
+		socketDir = "/cloudsql"
+	}
+
+	var dbURI string
+	dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, instanceConnectionName)
+
+	db, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an ent.Driver from `db`.
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	return ent.NewClient(ent.Driver(drv))
+}
+
+// mustGetEnv is a helper function for getting environment variables.
+// Displays a warning if the environment variable is not set.
+func mustGetenv(k string) string {
+	v := os.Getenv(k)
+	if v == "" {
+		log.Fatalf("Warning: %s environment variable not set.\n", k)
+	}
+	return v
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tmc/moderncrud
 go 1.16
 
 require (
-	entgo.io/ent v0.9.1 // indirect
+	entgo.io/ent v0.9.1
+	github.com/jackc/pgx/v4 v4.13.0
 	github.com/mattn/go-sqlite3 v1.14.8 // indirect
 )
