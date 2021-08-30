45671412cf72ded1376bc4ce222bd10ec4dfc81f
Travis Cline <travis.cline@gmail.com>
2021-08-30 10:21:38 -0700

    moderncrud: Add tools.go

---
+++ tools.go
@@ -0,0 +1,12 @@
+// +build tools
+
+// This file exists to manage the binaries relevant to this source tree via go modules.
+
+package moderncrud
+
+import (
+	_ "entgo.io/ent/cmd/ent"
+	_ "github.com/99designs/gqlgen"
+	_ "github.com/a8m/enter"
+	_ "honnef.co/go/tools/cmd/staticcheck"
+)
